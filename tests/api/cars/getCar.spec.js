81a122a9e50321678a1fe9bf7d62a5aa140d60d5
ekaterinarosina <kateeer94@gmail.com>
2024-08-20 18:47:09 +0300

    fix

--- tests/api/cars/getCar.spec.js
+++ tests/api/cars/getCar.spec.js
@@ -1,6 +1,7 @@
 import {expect, test} from "../../../src/fixtures/myFixtures";
 import CarsController from "../../../src/contrlollers/CarsController";
 import {CAR_BRANDS} from "../../../src/data/carBrands";
+import {CAR_MODELS} from "../../../src/data/carModels";
 
 test.describe("Get cars info with controller", ()=>{
 
@@ -39,33 +40,69 @@ test.describe("Get cars info with controller", ()=>{
     test(`Get car models`, async ({carsController})=>{
         const modelsList = await carsController.getCarModels()
         const data = await modelsList.json()
-        const expectedResponse = {
-            status: 'ok',
-            data: {
-                "id": CAR_BRANDS.Audi.id,
-                "title": CAR_BRANDS.Audi.title,
-                "logoFilename": CAR_BRANDS.Audi.logoFilename
-            }
-        };
-        expect(data).toEqual(expectedResponse);
+        const expectedBrandsArray = Object.values(CAR_MODELS).flatMap(brand =>
+            Object.values(brand)
+        );
+        expect(data.data).toEqual(expectedBrandsArray);
     })
 
     test(`Get car models by id`, async ({carsController})=>{
         const modelId = await carsController.getCarsModelsById(1)
         const data = await modelId.json()
-        expect(data.status, "Status should be ok").toBe('ok');
+        const expectedResponse = {
+            status: 'ok',
+            data: {
+                "id": CAR_MODELS.Audi.TT.id,
+                "carBrandId": CAR_MODELS.Audi.TT.carBrandId,
+                "title": CAR_MODELS.Audi.TT.title,
+            }
+        };
+        expect(data).toEqual(expectedResponse);
     })
 
     test(`Get current user cars`, async ({carsController, newCar: createdCar})=>{
         const carsList = await carsController.getCars()
         const data = await carsList.json()
-        expect(data.status, "Status should be ok").toBe('ok');
+        const expectedCarResponse = {
+            status: 'ok',
+            data: [
+                {
+                    id: createdCar.id,
+                    carBrandId: createdCar.carBrandId,
+                    carModelId: createdCar.carModelId,
+                    initialMileage: expect.any(Number),
+                    updatedMileageAt: expect.any(String),
+                    carCreatedAt: expect.any(String),
+                    mileage: expect.any(Number),
+                    brand: expect.any(String),
+                    model: expect.any(String),
+                    logo: expect.any(String),
+                }
+            ]
+        };
+        expect(data).toEqual(expectedCarResponse);
     });
 
     test(`Get car by id`, async ({carsController, newCar: createdCar})=>{
         const carId = await carsController.getCarById(createdCar.id)
         const data = await carId.json()
-        expect(data.status, "Status should be ok").toBe('ok');
+        const expectedResponse = {
+            status: 'ok',
+            data: [
+                {
+                    id: createdCar.id,
+                    carBrandId: createdCar.carBrandId,
+                    carModelId: createdCar.carModelId,
+                    initialMileage: expect.any(Number),
+                    updatedMileageAt: expect.any(String),
+                    carCreatedAt: expect.any(String),
+                    mileage: expect.any(Number),
+                    brand: expect.any(String),
+                    model: expect.any(String),
+                    logo: expect.any(String),
+                }
+            ]
+        };
+        expect(data).toEqual(expectedResponse);
     })
 })
-
